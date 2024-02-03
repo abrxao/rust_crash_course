74ccd6b39d0fee35f950b18f94f3cbcfe52d5119
abrxao <abrxao@gmail.com>
2024-02-03 16:02:18 -0300

    Option module finished

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 #![deny(clippy::all)]
 
+fn get_user_name() -> Result<String, ()> {
+    Ok("hello".to_string())
+}
+
+fn get_err_name() -> Result<String, ()> {
+    Err(())
+}
+fn get_double_name() -> Result<String, ()> {
+    let name_1 = get_user_name()?;
+    let name_2 = get_err_name()?;
+    Ok(format!("{} {}", name_1, name_2))
+}
 fn main() {
-    let number: Option<i32> = Some(12);
-    let mult_by_2 = number.map_or(20, |value| value * 2);
-    println!("{}", mult_by_2);
-    match number {
-        Some(number) => println!("{}", number),
-        None => println!("No number yet"),
+    let name = get_double_name();
+    match name {
+        Ok(name) => println!("testing {}", name),
+        Err(_) => println!("Some error ocurred"),
     }
 }
