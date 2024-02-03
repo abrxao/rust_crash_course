34d6fb61a67a06a5113423d5bd65c01a96c6eea4
abrxao <abrxao@gmail.com>
2024-02-03 14:58:03 -0300

    Option module finished

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let mut values = vec![1, 2, 3, 5, 8];
-    values.push(13);
-
-    for value in values.iter() {
-        let double_val: i32 = 2 * value;
-        println!("{}", double_val)
+    let number: Option<i32> = Some(12);
+    let mult_by_2 = number.map_or(20, |value| value * 2);
+    println!("{}", mult_by_2);
+    match number {
+        Some(number) => println!("{}", number),
+        None => println!("No number yet"),
     }
-    let m_b2: Vec<i32> = values.into_iter().map(|value| value * 2).collect();
-    println!("{:?}", m_b2)
 }
