f7c4a58b0ba6f39645cfc45ac66ed99a711b40e9
abrxao <abrxao@gmail.com>
2024-02-03 16:03:05 -0300

    Error Handling module finished

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn get_user_name() -> Result<String, ()> {
 fn get_err_name() -> Result<String, ()> {
     Err(())
 }
-fn get_double_name() -> Result<String, ()> {
+fn get_doubled_name() -> Result<String, ()> {
     let name_1 = get_user_name()?;
     let name_2 = get_err_name()?;
     Ok(format!("{} {}", name_1, name_2))
 }
 fn main() {
-    let name = get_double_name();
+    let name = get_doubled_name();
     match name {
         Ok(name) => println!("testing {}", name),
         Err(_) => println!("Some error ocurred"),
