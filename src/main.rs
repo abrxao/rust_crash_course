2f71985da223451345a7eebc2d0c5457d5618076
abrxao <abrxao@gmail.com>
2024-02-03 14:03:20 -0300

    finish collections module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 #![deny(clippy::all)]
 
-fn main() {}
-/*
-#[cfg(test)]
-mod tests {} */
+fn main() {
+    let mut values = vec![1, 2, 3, 5, 8];
+    values.push(13);
+
+    for value in values.iter() {
+        let double_val: i32 = 2 * value;
+        println!("{}", double_val)
+    }
+    let m_b2: Vec<i32> = values.into_iter().map(|value| value * 2).collect();
+    println!("{:?}", m_b2)
+}
