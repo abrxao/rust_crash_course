e751bcd445289402d0589a9b77b665c292473907
Abraão Albuquerque <abraao.hs2@gmail.com>
2024-02-04 19:16:48 -0300

    finishing lifitime module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 #![deny(clippy::all)]
 
-fn main() {}
+struct Person<'a>{
+    first_name: &'a str,
+    last_name: &'a str,
+}
+
+impl<'a> Person<'a> {
+    fn fisrt_char_of_first_name(&self) -> &str{
+        &self.first_name[0..1]
+    }
+    fn get_full_name(&self) -> String{
+        format!("{} {}", self.first_name, self.last_name)
+    }
+}
+
+enum Animal<'a>{
+    Dog{name: &'a str}
+}
+
+fn main() {
+    let person = Person{ first_name: "John", last_name: "Doe"};
+    let full_name = person.get_full_name();
+    println!("{:?}",full_name );
+}
 /*
 #[cfg(test)]
 mod tests {} */
